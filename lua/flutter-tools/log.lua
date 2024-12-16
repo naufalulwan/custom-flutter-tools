2fafb7685d07c98121e0cdfb0c3b207bdaf623ef
rdl-agen46 <naufalulwan63@gmail.com>
2024-12-16 15:33:43 +0700

    dah lah

--- lua/flutter-tools/log.lua
+++ lua/flutter-tools/log.lua
@@ -2,9 +2,9 @@ local lazy = require("flutter-tools.lazy")
 local ui = lazy.require("flutter-tools.ui") ---@module "flutter-tools.ui"
 local utils = lazy.require("flutter-tools.utils") ---@module "flutter-tools.utils"
 local config = lazy.require("flutter-tools.config") ---@module "flutter-tools.config"
-local log_highlighting = lazy.require("mtdl9/vim-log-highlighting")
 
 local api = vim.api
+local fmt = string.format
 
 local M = {
   --@type integer
@@ -13,7 +13,7 @@ local M = {
   win = nil,
 }
 
-M.filename = "flutter_dev_log"
+M.filename = "__FLUTTER_DEV_LOG__"
 
 --- check if the buffer exists if does and we
 --- lost track of it's buffer number re-assign it
@@ -31,8 +31,8 @@ end
 local function create(config)
   local opts = {
     filename = M.filename,
-    open_cmd = config.open_cmd,
     filetype = "log",
+    open_cmd = config.open_cmd,
     focus_on_open = config.focus_on_open,
   }
   ui.open_win(opts, function(buf, win)
@@ -53,25 +53,68 @@ function M.get_content()
   if M.buf then return api.nvim_buf_get_lines(M.buf, 0, -1, false) end
 end
 
-local function highlight_log(data) log_highlighting.highlight(data) end
+---Auto-scroll the log buffer to the end of the output
+---@param buf integer
+---@param target_win integer
+local function autoscroll(buf, target_win)
+  local win = utils.find(
+    api.nvim_tabpage_list_wins(0),
+    function(item) return item == target_win end
+  )
+  if not win then
+    win = utils.find(
+      api.nvim_tabpage_list_wins(0),
+      function(item) return vim.api.nvim_win_get_buf(item) == buf end
+    )
+    if win then M.win = win end
+  end
+  if not win then return end
+  -- if the dev log is focused don't scroll it as it will block the user from perusing
+  if api.nvim_get_current_win() == win then return end
+  local buf_length = api.nvim_buf_line_count(buf)
+  local success, err = pcall(api.nvim_win_set_cursor, win, { buf_length, 0 })
+  if not success then
+    ui.notify(fmt("Failed to set cursor for log window %s: %s", win, err), ui.ERROR, {
+      once = true,
+    })
+  end
+end
 
----Open a log showing the output from a command
----in this case flutter run
+---Add lines to a buffer
+---@param buf number
+---@param lines string[]
+local function append(buf, lines)
+  vim.bo[buf].modifiable = true
+  api.nvim_buf_set_lines(buf, -1, -1, true, lines)
+  vim.bo[buf].modifiable = false
+end
+
+--- Open a log showing the output from a command
+--- in this case flutter run
 ---@param data string
 function M.log(data)
   local opts = config.dev_log
   if opts.enabled then
     if not exists() then create(opts) end
     if opts.filter and not opts.filter(data) then return end
-
-    highlight_log(data)
-    vim.api.nvim_buf_set_lines(M.buf, -1, -1, false, { data })
+    append(M.buf, { data })
+    autoscroll(M.buf, M.win)
   end
+end
+
+function M.__resurrect()
+  local buf = api.nvim_get_current_buf()
+  vim.cmd("setfiletype log")
+  vim.bo[buf].modifiable = false
+  vim.bo[buf].modified = false
+  vim.bo[buf].buftype = "nofile"
 end
 
 function M.clear()
   if M.buf and api.nvim_buf_is_valid(M.buf) then
-    vim.api.nvim_buf_set_lines(M.buf, 0, -1, false, {}) -- Clear buffer content
+    vim.bo[M.buf].modifiable = true
+    api.nvim_buf_set_lines(M.buf, 0, -1, false, {})
+    vim.bo[M.buf].modifiable = false
   end
 end
 
@@ -84,6 +127,9 @@ M.toggle = function()
     end
   end
   create(config.dev_log)
+  -- Auto scroll to bottom
+  local buf_length = vim.api.nvim_buf_line_count(M.buf)
+  pcall(vim.api.nvim_win_set_cursor, M.win, { buf_length, 0 })
 end
 
 return M
--- lua/flutter-tools/ui.lua
+++ lua/flutter-tools/ui.lua
@@ -1,68 +1,153 @@
+local utils = require("flutter-tools.utils")
+local fmt = string.format
+
+---@enum EntryType
+local entry_type = {
+  CODE_ACTION = 1,
+  DEVICE = 2,
+}
+
+---@generic T
+---@alias SelectionEntry {text: string, type: EntryType, data: T}
+
 ---@enum
-local M = {}
+local M = {
+  ERROR = vim.log.levels.ERROR,
+  DEBUG = vim.log.levels.DEBUG,
+  INFO = vim.log.levels.INFO,
+  TRACE = vim.log.levels.TRACE,
+  WARN = vim.log.levels.WARN,
+}
+
+local api = vim.api
+local namespace_id = api.nvim_create_namespace("flutter_tools_popups")
+M.entry_type = entry_type
+
+function M.clear_highlights(buf_id, ns_id, line_start, line_end)
+  line_start = line_start or 0
+  line_end = line_end or -1
+  api.nvim_buf_clear_namespace(buf_id, ns_id, line_start, line_end)
+end
+
+--- @param buf_id number
+--- @param lines table[]
+--- @param ns_id integer?
+function M.add_highlights(buf_id, lines, ns_id)
+  if not buf_id then return end
+  ns_id = ns_id or namespace_id
+  if not lines then return end
+  for _, line in ipairs(lines) do
+    api.nvim_buf_add_highlight(
+      buf_id,
+      ns_id,
+      line.highlight,
+      line.line_number,
+      line.column_start,
+      line.column_end
+    )
+  end
+end
+
+--- check if there is a single non empty line
+--- in the list of lines
+--- @param lines table
+local function invalid_lines(lines)
+  for _, line in pairs(lines) do
+    if line ~= "" then return false end
+  end
+  return true
+end
 
 ---Post a message to UI so the user knows something has occurred.
 ---@param msg string | string[]
 ---@param level integer
 ---@param opts {timeout: number, once: boolean}?
 M.notify = function(msg, level, opts)
-  opts = opts or {}
-  local color = "Normal" -- Default color if no log level is found
-  if level == "ERROR" then
-    color = "Error"
-  elseif level == "WARNING" then
-    color = "WarningMsg"
-  elseif level == "INFO" then
-    color = "Directory"
-  elseif level == "DEBUG" then
-    color = "Comment"
+  opts, level = opts or {}, level or M.INFO
+  msg = type(msg) == "table" and utils.join(msg) or msg
+  if msg == "" then return end
+  local args = { title = "Flutter tools", timeout = opts.timeout, icon = "" }
+  if opts.once then
+    vim.notify_once(msg, level, args)
+  else
+    vim.notify(msg, level, args)
   end
-
-  -- Send colored output to the Neovim message area
-  vim.api.nvim_out_write(
-    string.format(
-      "\27[38;5;%dm%s\27[0m\n",
-      vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(color)), "fg"),
-      msg
-    )
-  )
-
-  -- Use Neovim's internal notify function
-  vim.notify(msg, level, opts)
 end
 
 ---@param opts table
----@param on_open fun(buf: integer, win: integer, job_id)
+---@param on_confirm function
+M.input = function(opts, on_confirm) vim.ui.input(opts, on_confirm) end
+
+--- @param items SelectionEntry[]
+--- @param title string
+--- @param on_select fun(item: SelectionEntry)
+local function get_telescope_picker_config(items, title, on_select)
+  local ok = pcall(require, "telescope")
+  if not ok then return end
+
+  local filtered = vim.tbl_filter(function(value) return value.data ~= nil end, items) --[[@as SelectionEntry[]]
+
+  return require("flutter-tools.menu").get_config(
+    vim.tbl_map(function(item)
+      local data = item.data
+      if item.type == entry_type.CODE_ACTION then
+        return {
+          id = data.title,
+          label = data.title,
+          command = function() on_select(data) end,
+        }
+      elseif item.type == entry_type.DEVICE then
+        return {
+          id = data.id,
+          label = data.name,
+          hint = data.platform,
+          command = function() on_select(data) end,
+        }
+      end
+    end, filtered),
+    { title = title }
+  )
+end
+
+---@alias PopupOpts {title:string, lines: SelectionEntry[], on_select: fun(item: SelectionEntry)}
+---@param opts PopupOpts
+function M.select(opts)
+  assert(opts ~= nil, "An options table must be passed to popup create!")
+  local title, lines, on_select = opts.title, opts.lines, opts.on_select
+  if not lines or #lines < 1 or invalid_lines(lines) then return end
+
+  vim.ui.select(lines, {
+    prompt = title,
+    kind = "flutter-tools",
+    format_item = function(item) return item.text end,
+    -- custom key for dressing.nvim
+    telescope = get_telescope_picker_config(lines, title, on_select),
+  }, function(item)
+    if not item then return end
+    on_select(item.data)
+  end)
+end
+
+---Create a split window
+---@param opts table
+---@param on_open fun(buf: integer, win: integer)
 ---@return nil
 function M.open_win(opts, on_open)
-  local win_opts = {
-    relative = "editor",
-    width = vim.o.columns,
-    height = vim.o.lines - 2,
-    col = 0,
-    row = 0,
-    style = "minimal",
-  }
+  local open_cmd = opts.open_cmd or "botright 30vnew"
+  local name = opts.filename or "__Flutter_Tools_Unknown__"
+  open_cmd = fmt("%s %s", open_cmd, name)
 
-  local buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
-  vim.api.nvim_buf_set_name(buf, opts.filename) -- Set the name of the buffer
-
-  -- Apply filetype
-  vim.bo[buf].filetype = opts.filetype or "log"
-
-  -- Open the window
-  local win = vim.api.nvim_open_win(buf, true, win_opts)
-
-  -- Apply any custom window options
-  if opts.open_cmd then vim.cmd(opts.open_cmd) end
-
-  if on_open then
-    -- Ensure `on_open` is called with the correct parameters
-    on_open(buf, win, opts.job_id)
+  vim.cmd(open_cmd)
+  local win = api.nvim_get_current_win()
+  local buf = api.nvim_get_current_buf()
+  vim.bo[buf].filetype = opts.filetype
+  vim.bo[buf].swapfile = false
+  vim.bo[buf].buftype = "nofile"
+  if on_open then on_open(buf, win) end
+  if not opts.focus_on_open then
+    -- Switch back to the previous window
+    vim.cmd("wincmd p")
   end
-
-  -- Focus window if required
-  if opts.focus_on_open then vim.api.nvim_set_current_win(win) end
 end
 
 return M
