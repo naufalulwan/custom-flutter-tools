877a78f9a4f05eba3d411c91ce2388dca245ebf1
rdl-agen46 <naufalulwan63@gmail.com>
2024-12-16 13:49:14 +0700

    Bismillah ke 2 kalinya

--- lua/flutter-tools/log.lua
+++ lua/flutter-tools/log.lua
@@ -37,6 +37,7 @@ local function create(config)
     filetype = "log",
     open_cmd = config.open_cmd,
     focus_on_open = config.focus_on_open,
+    shell = "pwsh",
   }
   ui.open_win(opts, function(buf, win)
     if not buf then
